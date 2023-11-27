11f9b471d19b270bbf1f607622dec0199db16a50
Faith Ye En Xin <faith.ye94@gmail.com>
2023-11-27 06:46:24 +0000

    completed project entries

--- ekyc_od_api/project_api/serializer.py
+++ ekyc_od_api/project_api/serializer.py
@@ -2,14 +2,6 @@ from rest_framework import serializers
 from .models import Project, ProjectEntry
 from users_api.serializers import UserSerializer
 
-class ProjectSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='project-detail')
-    user = UserSerializer(read_only=True) # this shows entire user detail
-    # user = serializers.PrimaryKeyRelatedField(read_only=True) # this shows user id only
-
-    class Meta:
-        model = Project
-        fields = ('url', 'id', 'project_name', 'user',)
 
 class ProjectEntrySerializer(serializers.ModelSerializer):
     # url = serializers.HyperlinkedIdentityField(view_name='project-detail')
@@ -17,3 +9,14 @@ class ProjectEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectEntry
         fields = ('id', 'project', 'email', 'first_name', 'last_name')  # Add other fields here as needed
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='project-detail')
+    user = UserSerializer(read_only=True) # this shows entire user detail
+    # user = serializers.PrimaryKeyRelatedField(read_only=True) # this shows user id only
+    entries = ProjectEntrySerializer(many=True, read_only=True) 
+
+
+    class Meta:
+        model = Project
+        fields = ('url', 'id', 'project_name', 'user', 'entries')
